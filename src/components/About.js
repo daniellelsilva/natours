c64032c191e5332e1dd0fdb2ebbaf9809ebb96fd
Danielle Silva <danielle.luisasilva@gmail.com>
2022-02-13 17:16:08 -0300

    adiciona about section

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import '../styles/abstracts/utilities.scss';
+import '../styles/components/button.scss';
+
+import '../styles/about.scss';
+
+export default function About() {
+  return (
+    <main>
+      <section className="section-about" >
+        <div className="u-center-text">
+          <h2 className="heading-secondary" >
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+        
+        <section className="about-text-image-content">
+          <div className="text-content">
+            <h3 className="heading-tertiary">
+              You are going to fall in love with nature
+            </h3>
+            <p className="paragraph">Lorem ipsum dolor sit amet. Aut quia cumque sed suscipit vitae qui porro veniam aut possimus adipisci hic sint eveniet sed dolor officiis! Ut velit rerum quo facere consequatur id culpa necessitatibus non reprehenderit mollitia est ipsum enim est tenetur pariatur. Id laborum voluptas et error illo ut necessitatibus dolore ut internos perferendis et esse sunt eos galisum ipsum 33 illo praesentium. </p>
+
+            <h3 className="heading-tertiary">
+              Live adventure like you never have before
+            </h3>
+            <p className="paragraph">Aut vero delectus vel accusamus consequatur qui magni itaque et molestias ipsa est assumenda asperiores. Qui sunt expedita quo quos ipsum et saepe dolorum et consequatur ratione. </p>
+
+            <a className="btn-text" href="#">Learn more</a>
+          </div>
+
+          <div className="image-content">
+            image content
+          </div>
+
+        </section>
+
+      </section>
+    </main>
+  )
+}
