2e69e9766ea00c03efac011898ab7bc930ffddf0
Danielle Silva <danielle.luisasilva@gmail.com>
2022-02-18 17:47:51 -0300

    adiciona comentario na section stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import maryImage from '../images/nat-8.jpg';
+import jackImage from '../images/nat-9.jpg';
 
 import '../styles/stories.scss';
 
@@ -13,9 +15,32 @@ export default function Stories() {
 
         <div className="stories-div">
           <div className="story">
-            text
+            <figure className="story-shape">
+              <img className="story-image" src={maryImage} alt="persoan on a tour" />
+              <figcaption className="story-caption">Mary Smith</figcaption>
+            </figure>
+            <div className="story-text">
+              <h3 className="heading-tertiary">I had the best week ever with my family</h3>
+              <p>Lorem ipsum dolor sit amet. Aut quia cumque sed suscipit vitae qui porro veniam aut possimus adipisci hic sint eveniet sed dolor officiis! Ut velit rerum quo facere consequatur id culpa necessitatibus non reprehenderit mollitia est ipsum enim est tenetur pariatur. Id laborum voluptas et error illo ut necessitatibus dolore ut internos perferendis et esse sunt eos galisum ipsum 33 illo praesentium.</p>
+            </div>
+          </div>
+
+          <div className="story">
+            <figure className="story-shape">
+              <img className="story-image" src={jackImage} alt="persoan on a tour" />
+              <figcaption className="story-caption">Jack Wilson</figcaption>
+            </figure>
+            <div className="story-text">
+              <h3 className="heading-tertiary">WOW! My life is completely different now</h3>
+              <p>Lorem ipsum dolor sit amet. Aut quia cumque sed suscipit vitae qui porro veniam aut possimus adipisci hic sint eveniet sed dolor officiis! Ut velit rerum quo facere consequatur id culpa necessitatibus non reprehenderit mollitia est ipsum enim est tenetur pariatur. Id laborum voluptas et error illo ut necessitatibus dolore ut internos perferendis et esse sunt eos galisum ipsum 33 illo praesentium.</p>
+            </div>
           </div>
         </div>
+
+        <div className="u-center-text">
+          <a href="#" className="btn-text all-stories-btn"> Read all stories</a>
+        </div>
+
     </section>
   )
 }
