9d2ffc1f817d1907afec5ff0c3a0c04e2f5915ae
Danielle Silva <danielle.luisasilva@gmail.com>
2022-02-25 20:02:27 -0300

    termina footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import fullLogo from '../images/logo-green-2x.png'
+import '../styles/footer.scss';
+import fullLogo from '../images/logo-green-2x.png';
 
 export default function Footer() {
   return (
@@ -9,9 +10,9 @@ export default function Footer() {
         <img src={fullLogo} alt="full logo" className="footer-logo" />
       </div>
 
-      <div>
-        <div className="footer-list-container">
-          <div>
+      <div className="footer-text">
+        <div className='col-1-of-2'>
+          <div className="footer-navigation">
             <ul className="footer-list">
               <li className="footer-item"><a href="#" className="footer-link">Company</a></li>
               <li className="footer-item"><a href="#" className="footer-link">Contact us</a></li>
@@ -20,6 +21,10 @@ export default function Footer() {
               <li className="footer-item"><a href="#" className="footer-link">Terms</a></li>
             </ul>
           </div>
+        </div>
+
+        <div className='col-1-of-2'>
+          <p className="footer-copyright">Site feito por <a href='#' className="footer-link">Danielle Silva</a> no curso de <a href='#' className="footer-link">CSS avançado e Sass</a> fornecido pela Udemy e ministrado por <a href='#' className="footer-link">Jonas Schmedtmann</a></p>
         </div>
       </div>
     </footer>
